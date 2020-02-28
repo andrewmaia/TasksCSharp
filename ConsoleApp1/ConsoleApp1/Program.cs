1198e8eb0c41808f6a4a9306a8d6401b41bd9a23
andrew.maia <andrew.maia@talktelecom.com.br>
2020-02-28 10:06:43 -0300

    Primeiro commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //Task1 é colocada para executar sem prender a thread principal
+        //Task2 é colocada para executar e prende a thread principal através do await
+        //Para entender bastar observar a ordem dos prints no console
+        static async Task Main(string[] args)
+        {
+            Task<int> task1 = Task.Run (()=>Task1());
+            Console.WriteLine("Chamou Task 1:" + DateTime.Now.ToString());
+            int retornoTask2 = await Task.Run(() => Task2());
+            Console.WriteLine("Chamou Task 2:" + DateTime.Now.ToString());
+            Console.WriteLine("Retorno Task 2:" + retornoTask2.ToString());
+            task1.Wait();//aguarda a task1 para mostrar seu resultado
+            Console.WriteLine("Retorno Task 1:" + task1.Result);
+            Console.ReadLine();
+        }
+
+
+
+        private static int Task1()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("Task 1:" + DateTime.Now.ToString());
+            return 1;
+        }
+
+        private static int Task2()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Task 2:" + DateTime.Now.ToString());
+            return 2;
+        }
+
+
+    }
+}
