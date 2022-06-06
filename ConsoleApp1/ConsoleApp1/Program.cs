1ec1bea899912d119afb2537ce5cf0d3a36cdb7e
Andrew Santos Maia <andrew.maia@talkcomm.com.br>
2022-06-06 11:19:20 -0300

    Métodos retornando tasks

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace ConsoleApp1
         //Para entender bastar observar a ordem dos prints no console
         static async Task Main(string[] args)
         {
+
             Task<int> task1 = Task.Run (()=>Task1());
             Console.WriteLine("Chamou Task 1:" + DateTime.Now.ToString());
             int retornoTask2 = await Task.Run(() => Task2());
@@ -18,6 +20,17 @@ namespace ConsoleApp1
             Console.WriteLine("Retorno Task 2:" + retornoTask2.ToString());
             task1.Wait();//aguarda a task1 para mostrar seu resultado
             Console.WriteLine("Retorno Task 1:" + task1.Result);
+
+            Console.WriteLine("-----------------------------");
+            //Mesma coisa só que as funções já retornam tasks
+            Task<int> task3 = Task3();
+            Console.WriteLine("Chamou Task 3:" + DateTime.Now.ToString());
+            int retornoTask4 = await Task4();
+            Console.WriteLine("Chamou Task 4:" + DateTime.Now.ToString());
+            Console.WriteLine("Retorno Task 4:" + retornoTask4.ToString());
+            task3.Wait();//aguarda a task3 para mostrar seu resultado
+            Console.WriteLine("Retorno Task 3:" + task3.Result);
+
             Console.ReadLine();
         }
 
@@ -37,6 +50,20 @@ namespace ConsoleApp1
             return 2;
         }
 
+        public static async Task<int> Task3()
+        {
+
+            await Task.Delay(10000);
+            Console.WriteLine("Task 3:" + DateTime.Now.ToString());
+            return 3;
+        }
+
+        public static async Task<int> Task4()
+        {
+            await Task.Delay(5000);
+            Console.WriteLine("Task 4:" + DateTime.Now.ToString());
+            return 4;
+        }
 
     }
 }
